cf98c6e67a330feb93afe5da084bbe6d4218e680
REIGN12 <3180105323@zju.edu.cn>
2021-01-15 19:28:55 +0800

    Adding Copy Constructor.

--- HLL.h
+++ HLL.h
@@ -15,6 +15,7 @@ class BaseNode
         BaseNode* next;
         BaseNode(BaseNode *f=nullptr, BaseNode* n=nullptr):forward{f},next{n}{}
         virtual ~BaseNode(){/*cout<<"delete basenode!\n";*/}
+        virtual BaseNode* clone() = 0;
         virtual ostream& to_stream(ostream& s) = 0;
         void link_f(BaseNode*f=nullptr){forward=f;}
         void link_n(BaseNode*n=nullptr){next=n;}
@@ -32,6 +33,11 @@ class Node: public BaseNode
         Node(T _data,BaseNode* f=nullptr, BaseNode* n=nullptr): data{_data},BaseNode(f,n) {}
         ~Node(){/*cout<<"delete node!\n";*/}
         ostream& to_stream(ostream& s){s << data;return s;}
+        BaseNode* clone()
+        {
+            BaseNode* p = new Node<T>(this->data);
+            return p;
+        }
 };
 
 class LL
@@ -39,6 +45,29 @@ class LL
     public:
         LL():root{nullptr}{}
         LL(BaseNode* _root):root{_root}{}
+        LL(LL& l)
+        {
+            this->root = l.root->clone();
+            if(l.root != nullptr)
+            {
+                BaseNode* tmp = l.root;
+                BaseNode* ntmp = this->root;
+                BaseNode* ntmp_n;
+                if(tmp->next != nullptr)
+                {
+                    do
+                    {
+                        tmp = tmp->next;
+                        ntmp_n = tmp->clone();
+                        ntmp->link_n(ntmp_n);
+                        ntmp_n->link_f(ntmp);
+                        ntmp = ntmp_n;
+                    } while (tmp->next != l.root);
+                    ntmp->link_n(this->root);
+                    this->root->link_f(ntmp);
+                }
+            }
+        }
         ~LL();
         // Construct from legal string
         LL(string& s);
